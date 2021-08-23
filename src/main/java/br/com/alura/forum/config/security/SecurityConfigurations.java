28ec546b72f05f9566a3b4160b50f402ab1e5eb3
Bruno Garcia Vasconcelos <bruno.vasconcelos@dataprev.gov.br>
2021-08-23 16:57:11 -0300

    Documentação da API com Swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -20,51 +20,58 @@ import br.com.alura.forum.repository.UsuarioRepository;
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
+	private static final String[] AUTH_WHITELIST = {
+			// -- Swagger UI v2
+			"/v2/api-docs", "/swagger-resources", "/swagger-resources/**", "/configuration/ui",
+			"/configuration/security", "/swagger-ui.html", "/webjars/**",
+			// -- Swagger UI v3 (OpenAPI)
+			"/v3/api-docs/**", "/swagger-ui/**"
+			// other public endpoints of your API may be appended to this array
+	};
+
 	@Autowired
 	private AutenticacaoService autenticacaoService;
-	
+
 	@Autowired
 	private TokenService tokenService;
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
+
 	@Override
 	@Bean
 	protected AuthenticationManager authenticationManager() throws Exception {
 		return super.authenticationManager();
 	}
-	
-	//Configurações de Autenticação
+
+	// Configurações de Autenticação
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
 	}
-	
-	//Configurações de Autorização
+
+	// Configurações de Autorização
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers(HttpMethod.GET, "/topicos").permitAll()
-			.antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
-			.antMatchers(HttpMethod.POST, "/auth").permitAll()
-			.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
-			.antMatchers("/h2-console/**").permitAll()
-			.anyRequest().authenticated()
-			.and().csrf().disable()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and().addFilterBefore(
-				new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), 
-				UsernamePasswordAuthenticationFilter.class);
+		http.authorizeRequests().antMatchers(HttpMethod.GET, "/topicos").permitAll()
+				.antMatchers(HttpMethod.GET, "/topicos/*").permitAll().antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+				.antMatchers("/h2-console/**").permitAll().anyRequest().authenticated().and().csrf()
+				.disable().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+				.addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository),
+						UsernamePasswordAuthenticationFilter.class);
 	}
-	
-	//Configurações de recursos estaticos(js, css, imagens, etc)
+
+	// Configurações de recursos estaticos(js, css, imagens, etc)
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-	
+		web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+		
+//		web.ignoring().antMatchers("/v3/api-docs", "/v2/api-docs", "/configuration/**", "/configuration/ui", "/swagger-resources/**",
+//				"/configuration/security", "/swagger-ui.html", "/webjars/**");
+
 	}
-	
+
 //	public static void main(String[] args) {
 //		System.out.println(new BCryptPasswordEncoder().encode("123456"));
 //	}
